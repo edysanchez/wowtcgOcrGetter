7d529094cc2a050ef8b0ab825eb3c8ea663eb317
edysanchez <edysanchez@gmail.com>
2013-04-26 01:19:50 +0200

    test pdfBox getting pdf pages

--- src/PdfBoxTest.java
+++ src/PdfBoxTest.java
@@ -1,38 +1,31 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.util.PDFTextStripper;
 
 public class PdfBoxTest {
-	public void getPdfText() {
+	public List getPdf() {
 		// Reads in pdf document
-		PDDocument pdDoc;
+		PDDocument pdDoc = null;
 		try {
-			pdDoc = PDDocument.load(new File(
-					"../res/wow_core_ocr_130410.pdf"));
+			pdDoc = PDDocument.load(new File("res/wow_core_ocr_130410.pdf"));
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
-		// Creates a new pdf document
-		PDDocument document = null;
-
-		// Adds specific page "i" where "i" is the page number and then saves
-		// the new pdf document
-		try {
-			document = new PDDocument();
-			document.addPage((PDPage) pdDoc.getDocumentCatalog().getAllPages()
-					.get(1));
-			document.save("file path" + "new document title" + ".pdf");
-			document.close();
-		} catch (Exception e) {
-		}
+	
+		return pdDoc.getDocumentCatalog().getAllPages();
 	}
 
 	public static void main(String[] args) {
+		PdfBoxTest pdf = new PdfBoxTest();
+		List pages=pdf.getPdf();
+		for(int index=0;index<pages.size();index++) {
+			System.out.println(pages.get(index).toString());
+		}
 
 		System.out.println("cucu");
 	}
